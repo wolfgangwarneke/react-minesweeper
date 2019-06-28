c7609899bd887dd6a590109ae0011ba5ff13c639
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2019-06-27 22:56:23 -0700

    scaffold project

---
+++ app/components/Minesweeper.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Minesweeper extends React.Component {
+  render() {
+    return (
+      <div>
+        this is Minesweeper
+      </div>
+    );
+  }
+}
+
+export default Minesweeper;
---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Minesweeper from './components/Minesweeper';
+
+const App = document.getElementById("app");
+
+ReactDOM.render(<Minesweeper />, App);
+
+console.log('hello');
