ff4791daf3bcec6f5a60cb30560d2846c778a8b0
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2019-06-27 23:55:25 -0700

    create basic Tile component

--- app/components/Minesweeper.js
+++ app/components/Minesweeper.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React from 'react';
+import Tile from './Tile';
 
 class Minesweeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: null
+    };
+  }
+  startGame() {
+    this.setState({
+      game: 'playing'
+    });
+  }
   render() {
-    return (
+    return this.state.game ?
       <div>
-        this is Minesweeper
+        <h1>this is minesweeper</h1>
+        <Tile />
+        <Tile />
+        <Tile />
       </div>
-    );
+      :
+      <div onClick={() => this.startGame()}>
+        Start
+      </div>  
+    ;
   }
 }
 
---
+++ app/components/Tile.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+document.oncontextmenu = () => false;
+
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      flagIndex: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.advanceFlagIndex = this.advanceFlagIndex.bind(this);
+    this.getDisplay = this.getDisplay.bind(this);
+  }
+  handleClick(e) {
+    if (e.type === 'click' && this.state.flagIndex === 0) console.log('left click');
+    if (e.type === 'contextmenu') this.advanceFlagIndex();
+  }
+  advanceFlagIndex() {
+    this.setState({
+      flagIndex: (this.state.flagIndex + 1) % 3
+    })
+  }
+  getDisplay() {
+    if (this.state.clicked) {
+      return 'has been clicked!';
+    }
+    return this.getFlagDisplay();
+  }
+  getFlagDisplay() {
+    return [
+      'blank',
+      'flagged',
+      'question'
+    ][this.state.flagIndex];
+  }
+  render() {
+    return (
+      <div onClick={this.handleClick} onContextMenu={this.handleClick}>
+        {this.getDisplay()}
+      </div>
+    );
+  }
+}
+
+export default Tile;
